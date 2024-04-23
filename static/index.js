23289055c16342d1d26fa90c6ce5d60d5c0f4872
JFinare <mate.kemenczey@gmail.com>
2024-04-23 12:36:55 +0200

    dom manip bugfixing

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 import data from './data.js';
-const root=document.querySelector("#root");
+const root =document.querySelector("#root")
 
 function buildMeUp(){
 let introEl =document.createElement("section");
@@ -32,27 +32,42 @@ function cardDealer(techno){
   imgTag.setAttribute('src',data.angular.image)
   imgTag.setAttribute('alt',data.angular.title)
   cardText.innerText=data.angular.text
+  cardImg.appendChild(imgTag)
   }else if(techno==="firebase"){
     imgTag.setAttribute('src',data.firebase.image)
     imgTag.setAttribute('alt',data.firebase.title)
     cardText.innerText=data.firebase.text
+    cardImg.appendChild(imgTag)
 
   }else if (techno==="nodejs"){
     imgTag.setAttribute('src',data.nodejs.image)
     imgTag.setAttribute('alt',data.nodejs.title)
     cardText.innerText=data.nodejs.text
+    cardImg.appendChild(imgTag)
 
   }else if(techno==="react"){
     imgTag.setAttribute('src',data.react.image)
     imgTag.setAttribute('alt',data.react.title)
     cardText.innerText=data.react.text
+    cardImg.appendChild(imgTag)
+
   }
-  cardBody.appendChild(cardImg,cardText);
-  theCard.appendChild(cardHead, cardBody);
+  cardBody.appendChild(cardImg);
+  cardBody.appendChild(cardText);
+
+  theCard.appendChild(cardHead);
+  theCard.appendChild(cardBody);
+
   return theCard;
 }
-contEl.appendChild(cardDealer("angular"),cardDealer("firebase"),cardDealer("nodejs"),cardDealer("react"))
-root.appendChild(introEl, contEl);
+contEl.appendChild(cardDealer("angular"))
+contEl.appendChild(cardDealer("firebase"))
+contEl.appendChild(cardDealer("nodejs"))
+contEl.appendChild(cardDealer("react"))
+
+root.appendChild(introEl);
+root.appendChild(contEl);
+
 
 }
 
