362accfcf6c5d647daae5e5859152e9ec79b66bb
JFinare <mate.kemenczey@gmail.com>
2024-04-26 13:36:50 +0200

    prettier

--- index.js
+++ index.js
@@ -6,26 +6,25 @@
 // - Words are separated by spaces, and the sentence may contain leading or trailing spaces.
 // - Consider only spaces as word separators (no punctuation splitting).
 export function reverseWords(sentence) {
-    let trimmed=sentence.trim();
-    const trimmedArr=trimmed.split(" ");
-    const splitArr=[];
-    let resultArray=[];
+  let trimmed = sentence.trim();
+  const trimmedArr = trimmed.split(" ");
+  const splitArr = [];
+  let resultArray = [];
 
-    for(let i=0;i<trimmedArr.length;i++){
-        if(trimmedArr[i]!==""){
-            splitArr.push(trimmedArr[i])
-        }
+  for (let i = 0; i < trimmedArr.length; i++) {
+    if (trimmedArr[i] !== "") {
+      splitArr.push(trimmedArr[i]);
     }
-    let j=splitArr.length-1;
+  }
+  let j = splitArr.length - 1;
 
-    for(let i=0;i<splitArr.length;i++){
-        resultArray.push(splitArr[j])
-        j--;
-    }
+  for (let i = 0; i < splitArr.length; i++) {
+    resultArray.push(splitArr[j]);
+    j--;
+  }
 
-    let resultSentence=resultArray.join(" ");
-    return resultSentence;
-
+  let resultSentence = resultArray.join(" ");
+  return resultSentence;
 }
 
 // Task: Word Frequency Counter
@@ -39,30 +38,29 @@ export function reverseWords(sentence) {
 // - The function should not count punctuation as part of the words.
 // - Return the result with words in lowercase.
 export function wordFrequency(text) {
-    const brokenDown=text.toLowerCase().replaceAll(/[,.'":?!]/g,"").split(" ");
-    const extraTrimmed=[]
-    for(let i=0;i<brokenDown.length;i++){
-        if(brokenDown[i]!==""){
-            extraTrimmed.push(brokenDown[i])
-        }
+  const brokenDown = text
+    .toLowerCase()
+    .replaceAll(/[,.'":?!]/g, "")
+    .split(" ");
+  const extraTrimmed = [];
+  for (let i = 0; i < brokenDown.length; i++) {
+    if (brokenDown[i] !== "") {
+      extraTrimmed.push(brokenDown[i]);
     }
-    const theResults={};
-    for (let i=0; i<extraTrimmed.length;i++){
-        let theWord=extraTrimmed[i];
-        if (theWord in theResults){
-            theResults[theWord]+=1;
-            }else{
-                theResults[theWord]=1;
-            }
-        }
-        return theResults
+  }
+  const theResults = {};
+  for (let i = 0; i < extraTrimmed.length; i++) {
+    let theWord = extraTrimmed[i];
+    if (theWord in theResults) {
+      theResults[theWord] += 1;
+    } else {
+      theResults[theWord] = 1;
     }
+  }
+  return theResults;
+}
 
 //console.log(wordFrequency("   This    is a   test   "))
-
-
-
-
 
 // Task: Top N Frequent Words
 // Write a function `topNFrequentWords(text, n)` that finds the top `n` most frequent words in a given text.
@@ -74,36 +72,40 @@ export function wordFrequency(text) {
 // - Words are separated by spaces, and the text may contain punctuation.
 // - Ignore punctuation, and consider only alphabetic characters for word separation.
 export function topNFrequentWords(text, n) {
-    const brokenDown=text.toLowerCase().replaceAll(/[,.'":?!]/g,"").split(" ");
-    let extraTrimmed=[]
-    for(let i=0;i<brokenDown.length;i++){
-        if(brokenDown[i]!==""){
-            extraTrimmed.push(brokenDown[i])
-        }
+  const brokenDown = text
+    .toLowerCase()
+    .replaceAll(/[,.'":?!]/g, "")
+    .split(" ");
+  let extraTrimmed = [];
+  for (let i = 0; i < brokenDown.length; i++) {
+    if (brokenDown[i] !== "") {
+      extraTrimmed.push(brokenDown[i]);
     }
-    extraTrimmed=extraTrimmed.sort()
-    const counted=[];
-    for(let i=0;i<extraTrimmed.length;i++){
-        let doesExist = counted.findIndex(existing => existing[0]===extraTrimmed[i]);
-        if (doesExist===-1){
-            counted.push([extraTrimmed[i],1])
-        }else{
-            counted[doesExist][1]++;
-        }
-        
+  }
+  extraTrimmed = extraTrimmed.sort();
+  const counted = [];
+  for (let i = 0; i < extraTrimmed.length; i++) {
+    let doesExist = counted.findIndex(
+      (existing) => existing[0] === extraTrimmed[i]
+    );
+    if (doesExist === -1) {
+      counted.push([extraTrimmed[i], 1]);
+    } else {
+      counted[doesExist][1]++;
     }
-    
-    counted.sort((a,b)=>b[1]-a[1])
-    counted.sort((a,b)=>{
-        if(a[1]===b[1]){
-        if (a[0]<b[0]){
-            return -1;
-        }
-        if (a[0]>b[0])
-        return 1;}
-    })
+  }
 
-    /*
+  counted.sort((a, b) => b[1] - a[1]);
+  counted.sort((a, b) => {
+    if (a[1] === b[1]) {
+      if (a[0] < b[0]) {
+        return -1;
+      }
+      if (a[0] > b[0]) return 1;
+    }
+  });
+
+  /*
     const orderByValue=[]
     counted.sort((a,b)=>{
         if (a.counted[1]<b.counted[1]){
@@ -115,8 +117,6 @@ export function topNFrequentWords(text, n) {
      })
      console.log(counted)
      */
-    const theResults=counted.slice(0,n)
-return theResults
-
+  const theResults = counted.slice(0, n);
+  return theResults;
 }
-
--- static/index.js
+++ static/index.js
@@ -1,77 +1,69 @@
-import data from './data.js';
-const root =document.querySelector("#root")
+import data from "./data.js";
+const root = document.querySelector("#root");
 
-function buildMeUp(){
-let introEl =document.createElement("section");
-introEl.className="intro"
-let introHead=document.createElement("h1");
-introHead.className="intro__header"
-introEl.appendChild(introHead);
+function buildMeUp() {
+  let introEl = document.createElement("section");
+  introEl.className = "intro";
+  let introHead = document.createElement("h1");
+  introHead.className = "intro__header";
+  introEl.appendChild(introHead);
 
+  let contEl = document.createElement("section");
+  contEl.className = "container";
 
-let contEl=document.createElement("section");
-contEl.className="container";
+  function cardDealer(techno) {
+    let theCard = document.createElement("div");
+    theCard.className = "card";
 
-function cardDealer(techno){
-  let theCard=document.createElement("div");
-  theCard.className="card";
+    let cardHead = document.createElement("div");
+    let cardBody = document.createElement("div");
+    cardHead.className = "card__header";
+    cardBody.className = "card__body";
 
-  let cardHead=document.createElement("div");
-  let cardBody=document.createElement("div");
-  cardHead.className="card__header";
-  cardBody.className="card__body";
+    let cardImg = document.createElement("div");
+    cardImg.className = "card__body__image";
 
-  let cardImg=document.createElement("div");
-  cardImg.className="card__body__image";
+    let imgTag = document.createElement("img");
+    let cardText = document.createElement("div");
+    cardText.className = "card__body__text";
 
-  let imgTag=document.createElement("img");
-  let cardText=document.createElement("div");
-  cardText.className="card__body__text"
-  
-  if(techno==="angular"){
-  imgTag.setAttribute('src',data.angular.image)
-  imgTag.setAttribute('alt',data.angular.title)
-  cardText.innerText=data.angular.text
-  cardImg.appendChild(imgTag)
-  }else if(techno==="firebase"){
-    imgTag.setAttribute('src',data.firebase.image)
-    imgTag.setAttribute('alt',data.firebase.title)
-    cardText.innerText=data.firebase.text
-    cardImg.appendChild(imgTag)
+    if (techno === "angular") {
+      imgTag.setAttribute("src", data.angular.image);
+      imgTag.setAttribute("alt", data.angular.title);
+      cardText.innerText = data.angular.text;
+      cardImg.appendChild(imgTag);
+    } else if (techno === "firebase") {
+      imgTag.setAttribute("src", data.firebase.image);
+      imgTag.setAttribute("alt", data.firebase.title);
+      cardText.innerText = data.firebase.text;
+      cardImg.appendChild(imgTag);
+    } else if (techno === "nodejs") {
+      imgTag.setAttribute("src", data.nodejs.image);
+      imgTag.setAttribute("alt", data.nodejs.title);
+      cardText.innerText = data.nodejs.text;
+      cardImg.appendChild(imgTag);
+    } else if (techno === "react") {
+      imgTag.setAttribute("src", data.react.image);
+      imgTag.setAttribute("alt", data.react.title);
+      cardText.innerText = data.react.text;
+      cardImg.appendChild(imgTag);
+    }
+    cardBody.appendChild(cardImg);
+    cardBody.appendChild(cardText);
 
-  }else if (techno==="nodejs"){
-    imgTag.setAttribute('src',data.nodejs.image)
-    imgTag.setAttribute('alt',data.nodejs.title)
-    cardText.innerText=data.nodejs.text
-    cardImg.appendChild(imgTag)
+    theCard.appendChild(cardHead);
+    theCard.appendChild(cardBody);
 
-  }else if(techno==="react"){
-    imgTag.setAttribute('src',data.react.image)
-    imgTag.setAttribute('alt',data.react.title)
-    cardText.innerText=data.react.text
-    cardImg.appendChild(imgTag)
-
+    return theCard;
   }
-  cardBody.appendChild(cardImg);
-  cardBody.appendChild(cardText);
+  contEl.appendChild(cardDealer("angular"));
+  contEl.appendChild(cardDealer("firebase"));
+  contEl.appendChild(cardDealer("nodejs"));
+  contEl.appendChild(cardDealer("react"));
 
-  theCard.appendChild(cardHead);
-  theCard.appendChild(cardBody);
-
-  return theCard;
+  root.appendChild(introEl);
+  root.appendChild(contEl);
 }
-contEl.appendChild(cardDealer("angular"))
-contEl.appendChild(cardDealer("firebase"))
-contEl.appendChild(cardDealer("nodejs"))
-contEl.appendChild(cardDealer("react"))
-
-root.appendChild(introEl);
-root.appendChild(contEl);
-
-
-}
-
-
 
 function main() {
   console.log(data);
